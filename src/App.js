9d14a77e3673fda27c48e6c2cb08055eda00bbcd
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-10-05 15:12:25 +0100

    implement AppContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import ExampleModule from '@govuk-frederic/example-module';
+import AppContainer from '@govuk-frederic/app-container';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ExampleModule />
+        <AppContainer modules={[
+          {
+            example: {
+              render: () => <div>hi</div>
+            }
+          }
+        ]} />
       </div>
     );
   }
