d7c9136bca9b31fdcdb427d71fbf35f3fa9aa1a2
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-10-23 12:47:06 +0100

    toast notifications, top nav, govuk-react and frederic

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ Will show how to use the Frederic UI component library and App Container to buil
 
 # Frederic projects:
 
-https://github.com/stevesims/govuk-frederic
-https://github.com/penx/govuk-frederic-server
-https://github.com/penx/govuk-frederic-app-container
-https://github.com/penx/govuk-frederic-example
-https://github.com/penx/govuk-frederic-example-module
+- https://github.com/stevesims/govuk-frederic
+- https://github.com/penx/govuk-frederic-server
+- https://github.com/penx/govuk-frederic-app-container
+- https://github.com/penx/govuk-frederic-example
+- https://github.com/penx/govuk-frederic-example-module
+- https://github.com/penx/govuk-frederic-sample-data
+- https://github.com/penx/govuk-frederic-api
--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import AppContainer from '@govuk-frederic/app-container';
+import { Button } from 'govuk-react';
+import { ToastConsumer } from 'react-toast-notifications';
 
 import asyncComponent from './components/async-component';
+
+import './styles.css'
 
 const ExampleModule = asyncComponent(() => import('@govuk-frederic/example-module'));
 const ExampleModule2 = asyncComponent(() => import('@govuk-frederic/example-module-2'));
@@ -14,12 +18,24 @@ class App extends Component {
           {
             name: 'example',
             displayName: 'Example',
-            render: () => <div><ExampleModule /></div>
+            render: () => <div>
+              <ExampleModule />
+              <ToastConsumer>
+                {({add}) => {
+                  return <Button onClick={(e) => add(`Notified by ${e.target}`, {
+                    appearance: 'success',
+                    autoDismiss: true,
+                  })}>
+                    Notify me
+                  </Button>
+               }}
+              </ToastConsumer>
+            </div>
           },
           {
             name: 'example2',
             displayName: 'Example2',
-            render: () => <div><ExampleModule2 /></div>
+            render: () => <ExampleModule2 />
           }
         ]} />
       </div>
